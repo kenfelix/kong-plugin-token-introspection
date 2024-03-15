99bfeb2cfde658e428cc2d34891350b5d5970520
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-03-15 23:12:41 +0100

    Debug: tweaking code to fit purpose

--- kong/plugins/token-introspection/handler.lua
+++ kong/plugins/token-introspection/handler.lua
@@ -8,7 +8,7 @@ local kong_meta = require "kong.meta"
 -- issue token introspection request
 local function do_introspect_access_token(access_token, config)
   print("sending request to auth server...")
-  print(config.introspection_ssl_verify)
+  print(config.introspection_endpoint)
   local res, err = http:new():request_uri(config.introspection_endpoint, {
     ssl_verify = config.introspection_ssl_verify,
     method = "POST",
